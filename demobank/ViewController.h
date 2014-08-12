7665ab1c2b6dbba6e438224eb1eb4ffa173fb68d
Andrey Trifonov <Trifonov@rutoken.ru>
2014-08-12 12:10:17 +0400

    Create empty ios app

---
+++ demobank/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  demobank
+//
+//  Created by Андрей Трифонов on 12.08.14.
+//  Copyright (c) 2014 Aktiv Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
