ef499476796625d73dc883d36682bc3a7e76bd2e
Andrey Trifonov <Trifonov@rutoken.ru>
2014-08-22 14:23:31 +0400

    Add empty singletone realization of TokenManager

--- demobank/TokenManager.h
+++ demobank/TokenManager.h
@@ -2,6 +2,28 @@
 
 #import <Foundation/Foundation.h>
 
+#import <RtPKCS11ECP/rtpkcs11.h>
+
+#import "Token.h"
+
+@class Pkcs11EventHandler;
+
+typedef NS_ENUM(NSInteger, EventType) {
+	TA, //Token added
+	TR, //Token removed
+	TIL, //Token's information loaded
+	TILF //Token's information loading failed
+};
+
 @interface TokenManager : NSObject
+
++(TokenManager*)sharedInstance;
+
+-(void)start;
+-(void)stop;
+-(NSArray*)serials;
+-(Token*)tokenForSerial:(NSString*)serial;
+-(void)proccessEventTokenAddedAtSlot:(CK_SLOT_ID)id;
+-(void)proccessEventTokenRemovedAtSlot:(CK_SLOT_ID)id;
 
 @end
--- demobank/TokenManager.m
+++ demobank/TokenManager.m
@@ -4,4 +4,38 @@
 
 @implementation TokenManager
 
++(TokenManager*)sharedInstance{
+	static dispatch_once_t runOnce;
+	static id sharedObject = nil;
+	dispatch_once(&runOnce, ^{
+        sharedObject = [[self alloc] init];
+    });
+	
+	return sharedObject;
+}
+
+-(void)start{
+	//Start all activities for monitoring tokens' events
+}
+
+-(void)stop{
+	//Stop all activities for monitoring tokens' events
+}
+
+-(NSArray*)serials{
+	//Return serials for all know tokens
+	return nil;
+}
+-(Token*)tokenForSerial:(NSString*)serial{
+	//Return token for given serial
+	return nil;
+}
+
+-(void)proccessEventTokenAddedAtSlot:(CK_SLOT_ID)id{
+	//Proccess token adding here
+}
+-(void)proccessEventTokenRemovedAtSlot:(CK_SLOT_ID)id{
+	//Proccess token removing here
+}
+
 @end
