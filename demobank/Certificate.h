21d46184170c688fdf44af452331fa56bcb463fc
Andrey Trifonov <Trifonov@rutoken.ru>
2014-08-26 17:23:27 +0400

    Add stub for Certificate class

---
+++ demobank/Certificate.h
@@ -0,0 +1,9 @@
+//  Copyright (c) 2014 Aktiv Co. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+@interface Certificate : NSObject
+
+-(id)init;
+
+@end
---
+++ demobank/Certificate.m
@@ -0,0 +1,12 @@
+//  Copyright (c) 2014 Aktiv Co. All rights reserved.
+
+#import "Certificate.h"
+
+@implementation Certificate
+
+-(id)init{
+    self = [super init];
+    return self;
+}
+
+@end
