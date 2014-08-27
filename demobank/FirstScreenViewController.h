a1e5924391d85964735bc6a4b2babbf56e35e638
Andrey Trifonov <Trifonov@rutoken.ru>
2014-08-27 18:38:15 +0400

    Improve UI of FirstViewController: Show token info and some state notifications

--- demobank/FirstScreenViewController.h
+++ demobank/FirstScreenViewController.h
@@ -11,7 +11,11 @@
 	BluetoothDelegate* _delegate;
 	CBCentralManager* _manager;
 	TokenManager* _tokenManager;
+    NSNumber* _activeTokenHandle;
     __weak IBOutlet UIImageView *_headerImage;
     __weak IBOutlet UIImageView *_tokenImage;
+    __weak IBOutlet UILabel *_tokenModelLabel;
+    __weak IBOutlet UILabel *_tokenSerialNumberLabel;
+    __weak IBOutlet UILabel *_statusInfoLabel;
 }
 @end
--- demobank/FirstScreenViewController.m
+++ demobank/FirstScreenViewController.m
@@ -54,27 +54,45 @@
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(tokenAddingFailed:)
 												 name:@"TokenAddingFailed" object:_tokenManager];
 	
+    _activeTokenHandle = nil;
+    [_tokenModelLabel setText:@""];
+    [_tokenSerialNumberLabel setText:@""];
+    [_statusInfoLabel setText:@""];
+    
 	_delegate = [[BluetoothDelegate alloc] init];
 	_manager = [[CBCentralManager alloc] initWithDelegate:_delegate queue:nil];
 	_tokenManager = [TokenManager sharedInstance];
+
 	[_tokenManager start];
 }
 
 - (void)bluetoothWasPoweredOn:(NSNotification*)notification {
 	//handle bluetooth powering ON here
+    [_statusInfoLabel setText:@"Для работы с демобанком подключите токен"];
 	NSLog(@"Bluetooth was powered on");
 }
 
 - (void)bluetoothWasPoweredOff:(NSNotification*)notification {
 	//handle bluetooth powering OFF here
+    [_statusInfoLabel setText:@"Для работы с демобанком включите bluetooth"];
     NSLog(@"Bluetooth was powered off");
 }
 
 - (void)tokenWasAdded:(NSNotification*)notification {
 	//handle token adding here
-	NSDictionary* userInfo = [notification userInfo];
-	NSNumber* handle = [userInfo objectForKey:@"handle"];
-	Token* token = [_tokenManager tokenForId:handle];
+    NSDictionary* userInfo = [notification userInfo];
+    NSNumber* handle = [userInfo objectForKey:@"handle"];
+    Token* token = [_tokenManager tokenForId:handle];
+    
+    if(nil == _activeTokenHandle) {
+        _activeTokenHandle = handle;
+        [_tokenModelLabel setText:[token model]];
+        NSUInteger decSerial;
+        [[NSScanner scannerWithString:[token serialNumber]] scanHexInt:&decSerial];
+        NSString* decSerialString = [NSString stringWithFormat:@"0%u", decSerial];
+        [_tokenSerialNumberLabel setText:[decSerialString substringFromIndex:[decSerialString length]-5]];
+        [_statusInfoLabel setText:@""];
+    }
     
     NSLog(@"Info for token with handle %d was loaded: \"Model: %@, Serial: %@, Label: %@\"", [handle intValue], [token model], [token serialNumber], [token label]);
 }
@@ -83,6 +101,12 @@
 	//handle token removing here
 	NSDictionary* userInfo = [notification userInfo];
 	NSNumber* handle = [userInfo objectForKey:@"handle"];
+    if(handle == _activeTokenHandle){
+        _activeTokenHandle = nil;
+        [_statusInfoLabel setText:@"Для работы с демобанком подключите токен"];
+        [_tokenModelLabel setText:@""];
+        [_tokenSerialNumberLabel setText:@""];
+    }
     
     NSLog(@"Token with handle %d was removed", [handle intValue]);
 }
