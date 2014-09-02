7b52ebf2f23515a0e350c647baa23debc1cc0025
Andrey Trifonov <Trifonov@rutoken.ru>
2014-09-02 12:29:12 +0400

    Add details for payments - payment info VC

---
+++ demobank/PaymentsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaymentsTableViewController.h
+//  demobank
+//
+//  Created by Андрей Трифонов on 02.09.14.
+//  Copyright (c) 2014 Aktiv Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentsTableViewController : UITableViewController
+
+@end
