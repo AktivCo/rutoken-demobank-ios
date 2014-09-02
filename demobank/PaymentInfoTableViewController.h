dae585fb68200ad42ca096319f1b3bc1cfc0b649
Andrey Trifonov <Trifonov@rutoken.ru>
2014-09-02 13:08:01 +0400

    Remove unused code from Payments VCs

--- demobank/PaymentInfoTableViewController.h
+++ demobank/PaymentInfoTableViewController.h
@@ -1,10 +1,4 @@
-//
-//  PaymentInfoTableViewController.h
-//  demobank
-//
-//  Created by Андрей Трифонов on 02.09.14.
 //  Copyright (c) 2014 Aktiv Co. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
